7f49cae84d3e7444ed16dd5a4a60df93a4d31fee
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-06-15 19:10:30 +0100

    url added from edamam and api key added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import { APP_ID, APP_KEY} from './key';
 
 function App() {
+  let query = "apple";
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&imageSize=REGULAR`;
+
   return (
     <div className="App">
       {/* <header className="App-header">    
