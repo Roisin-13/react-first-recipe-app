96855b96c286f97ac969e7e041fefde8ab095f36
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-07-07 15:15:51 +0100

    axios added to handle requests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import { APP_ID, APP_KEY} from './key';
+import Axios from 'axios';
 
 function App() {
   let query = "apple";
   const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&imageSize=REGULAR`;
+
+  async function getRecipe() {
+    let recipe = await Axios;
+  }
 
   return (
     <div className="App">
