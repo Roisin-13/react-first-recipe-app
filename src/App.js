3fcfef72ec93a84c14d5ec893d9405e7c502bf60
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-07-13 16:51:33 +0100

    call to api added, can see data in console

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import { APP_ID, APP_KEY} from './key';
+import { APP_ID , APP_KEY} from './key';
 import Axios from 'axios';
 
 function App() {
-  let query = "apple";
+  let query = `apple`;
   const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&imageSize=REGULAR`;
 
   async function getRecipe() {
-    let recipe = await Axios;
+    let recipe = await Axios.get(url);
+    console.log(recipe.data);
   }
 
   return (
@@ -15,6 +16,11 @@ function App() {
       {/* <header className="App-header">    
       </header> */}
     <h1>Recipe Finder</h1>
+    <form className='searchForm'>
+        <input type="text" placeholder='search ingredient'></input>
+       
+    </form>
+      <button onClick={getRecipe}>plz click</button>
     </div>
   );
 }
