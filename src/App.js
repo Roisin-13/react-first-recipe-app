8548d77945add98494d90e0048aef18fdd93ec2e
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-07-14 17:02:19 +0100

    function added to form to accept text input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import './App.css';
 import { APP_ID , APP_KEY} from './key';
 import Axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-  let query = `apple`;
+  const [query, setQuery] = useState("");
+  //let query = `apple`;
   const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&imageSize=REGULAR`;
 
   async function getRecipe() {
     let recipe = await Axios.get(url);
     console.log(recipe.data);
   }
-
+  const handleClick = (event) => {
+    event.preventDefault();
+    getRecipe();
+    document.getElementsByClassName("searchIngredient")[0].value="";
+  }
   return (
     <div className="App">
       {/* <header className="App-header">    
       </header> */}
     <h1>Recipe Finder</h1>
-    <form className='searchForm'>
-        <input type="text" placeholder='search ingredient'></input>
-       
+    <form className="searchForm">
+        <input type="text" className="searchIngredient" placeholder="search ingredient" value={query} onChange={(event) => setQuery(event.target.value)}></input>
+        <button type="submit" value="plz click" onClick={handleClick} >plz click</button>
     </form>
-      <button onClick={getRecipe}>plz click</button>
+      {/* <button onClick={getRecipe}>plz click</button> */}
     </div>
   );
 }
