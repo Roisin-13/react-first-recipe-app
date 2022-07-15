d5e3d02147477e23e7971fb1212de28f16ac2faf
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-07-15 19:05:48 +0100

    recipes added to front end to be seen by user

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { useState } from 'react';
 
 function App() {
   const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
 
   const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&imageSize=REGULAR`;
 
   async function getRecipe() {
     let recipe = await Axios.get(url);
+    setRecipes(recipe.data.hits);
     console.log(recipe.data);
   }
   const handleClick = (event) => {
@@ -17,13 +19,26 @@ function App() {
     getRecipe();
     document.getElementsByClassName("searchIngredient")[0].value="";
   }
+
   return (
     <div className="App">
     <h1>Recipe Finder</h1>
     <form className="searchForm">
-        <input type="text" className="searchIngredient" placeholder="search ingredient" value={query} onChange={(event) => setQuery(event.target.value)} onSubmit={handleClick}></input>
+        <input type="text" className="searchIngredient" placeholder="search ingredient" value={query} onChange={(event) => setQuery(event.target.value)}></input>
         <button type="submit" value="plz click" onClick={handleClick} >find recipes</button>
     </form>
+    <div>
+      {recipes.map(results => {
+        return (
+          <>
+          <a href={results.recipe.url}>
+          <img src={results.recipe.image}></img>
+            </a>
+          <p>{results.recipe.label}</p>
+          </>
+          );
+      })}
+    </div>
     </div>
   );
 }
