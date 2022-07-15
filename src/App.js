5ee771c129f1333c0307f19ae11bcbc4ebb5f0dc
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-07-15 15:45:14 +0100

    code cleaned

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 function App() {
   const [query, setQuery] = useState("");
-  //let query = `apple`;
+
   const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&imageSize=REGULAR`;
 
   async function getRecipe() {
@@ -19,14 +19,11 @@ function App() {
   }
   return (
     <div className="App">
-      {/* <header className="App-header">    
-      </header> */}
     <h1>Recipe Finder</h1>
     <form className="searchForm">
-        <input type="text" className="searchIngredient" placeholder="search ingredient" value={query} onChange={(event) => setQuery(event.target.value)}></input>
-        <button type="submit" value="plz click" onClick={handleClick} >plz click</button>
+        <input type="text" className="searchIngredient" placeholder="search ingredient" value={query} onChange={(event) => setQuery(event.target.value)} onSubmit={handleClick}></input>
+        <button type="submit" value="plz click" onClick={handleClick} >find recipes</button>
     </form>
-      {/* <button onClick={getRecipe}>plz click</button> */}
     </div>
   );
 }
